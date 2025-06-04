9bdb43ea06ac2743cc9e63769b95d980f0cac6c8
suzan-verse <khadkasuzon7@gmail.com>
2025-06-04 20:16:23 +0545

    up

--- basics.py
+++ basics.py
@@ -60,7 +60,69 @@ print(bool("Hero"))   #True
 # converting anything to boolean 
 # here "0","0.0","[]","None"  gives false and other gives True
 
+# conditional statement  (if-elif-else)
+# condition is any expression that evaluate to either True/False. Baically, it is a test where the output is yes/no.
 
+# for example   5>3 (True), "cat"=="dog" (False), 3!=5 (True), "5"*3 == 111*5 (True)
+# lets use if elif else statement in python
+
+balance= int(input("Entre the balance:"))   #example 1
+
+if balance >=10000:
+    print("High Income")
+
+elif 5000<= balance <=10000:
+    print("Average Income")
+
+else :
+    print("Below average")
+
+
+num1= int(input("Entre the first number:"))  #example 2
+num2= int(input("Entre the second number:"))
+
+if num1>num2 :
+    print(f"The number {num1} is greater.")
+
+elif num2 > num1 :
+    print(f"The number {num2} is greater.")
+
+else :
+    print(f"The number {num1} and {num2} are equal. ")    
+ 
+income = 50000   #nested if (example 3)
+age = 30
+
+if income > 40000:
+    if age > 22:
+        print("Eligible for loan")
+    else:
+        print("Too young")
+else:
+    print("Income too low")
+
+# operators (comparision & logical)
+# == (equal to) , != (not equal to) , >= (greater or euqal to) , <= (less 0r equal to)
+
+# and operator (only runs if both conditions are true)
+# or operator (runs even if only one condition is true)
+# not operator (reverse the condition true becomes false and vice -versa)
+
+age =int(input("Entre the age:"))  #example 1 and operator
+citizen = False
+
+if age>18 and citizen:
+    print("you can vote.")
+else:
+    print("you are not eligible.")
+
+
+hey=int(input("Entre the number:"))  #example 2 or operator
+
+if hey <1 or hey>10:
+    print("out of range.")
+else:
+    print("With in range.")
 
 
 
---
+++ marksproj.py
@@ -0,0 +1,25 @@
+# name= str(input("Entre the Student Name :"))
+# marks= int(input("Entre the Student Marks :"))
+
+# if marks >= 90:
+#     print(f"{name},The Grade is A+. Well done!")
+
+# elif 80 <= marks <=89 :
+#     print(f"{name},The Grade is A. You are on right track. Well done!")
+
+# elif 70 <= marks <=79 :
+#     print(f"{name},The Grade is B+.You must work hard. Keep up the work!") 
+
+# elif 60 <= marks <= 69:
+#     print (f"{name},The Grade is B. You need more focous on your study. Study hard!")
+
+# else:
+#     print(f"{name},You are Not Graded. Give your more time into studies!")      
+
+
+print("5"*3)
+print(111*5)
+
+            
+
+    
